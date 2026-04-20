d4a5f9821ff6fcfbc53a7a7c7f8b4dc16eb780dc
Jenifer Magbanua <jenifermagbanua99@gmail.com>
2026-04-20 20:33:31 +0800

    Update image path for profile picture

--- portfolio_app/pages/1_🏠_Home.py
+++ portfolio_app/pages/1_🏠_Home.py
@@ -4,7 +4,7 @@ st.title("🏠 Home")
 st.header("Hi I'm Jenifer")
 st.write("Aspiring Developer | Designer | Tech Enthusaist")
 
-st.image("C:/Users/jenif/OneDrive/图片/images_profile pic.jpg", caption="My Banner")
+st.image("portfolio_app/images_profile pic.jpg", caption="My Banner")
 st.info("Welcome to my portfolio! Explore my work and skills")
 
 
