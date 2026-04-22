375f74d7e9e15bbec548acbef9e1e7d0f4006fb2
Jenifer Magbanua <jenifermagbanua99@gmail.com>
2026-04-22 11:21:39 +0800

    Add files via upload

---
+++ portfolio_app/Dashboard.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Portfolio",
+    page_icon="🌐",
+    layout="wide"
+    )
+
+st.title("Welcome to My Portfolio App")
+st.write("Use the sidebar to navigate through different sections.")
+st.write("🏠 Home")
+st.write("ℹ️ About")
+st.write("💡 Skills")
+st.write("📂 Projects")
+st.write("📞 Contact")
