23c6695e35f2c355fd839cf9d0bc8e3599baa432
Jenifer Magbanua <jenifermagbanua99@gmail.com>
2026-04-22 11:27:33 +0800

    Update 4_📂_Projects.py

--- portfolio_app/pages/4_📂_Projects.py
+++ portfolio_app/pages/4_📂_Projects.py
@@ -9,7 +9,7 @@ projects = {
     },
     "📊 ScanSafe System": {
         "desc": "This System is a digital solution designed to improve safety, monitoring, and record management using QR code technology. It allows institutions such as schools, barangays, or offices to efficiently track individuals’ attendance and movement in real time.",
-        "img": "C:/Users/jenif/OneDrive/图片/Screenshots 1/Screenshot 2025-12-13 135827.png"
+        "img": "portfolio_app/Screenshot 2025-12-13 135827.png"
     },
     "🏫 Barangay Record System": {
         "desc": "A system is a digital solution designed to make barangay services faster, more organized, and accessible through mobile devices like smartphones or tablets. Instead of relying on manual records (paper-based), the system stores and manages data electronically, allowing barangay staff and sometimes residents to access important information anytime.",
